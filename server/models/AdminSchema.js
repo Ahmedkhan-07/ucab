22f6eb2554022d55d78431077b0629d113190cf4
Harshitha <harshithauppeti@gmail.com>
2026-06-30 14:47:03 +0530

    updated schemas

---
+++ server/models/AdminSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const AdminSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  }
+}, { timestamps: true })
+
+const Admin = mongoose.model('Admin', AdminSchema)
+
+module.exports = Admin;
